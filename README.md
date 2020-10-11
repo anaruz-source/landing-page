c5996404d3c1caba731851bf8e4cc762eabb6f95
root <root@localhost.localdomain>
2020-10-11 19:46:07 +0100

    first commit of landing page, index.html, js/app.js, css/styles.css README.md

---
+++ README.md
@@ -0,0 +1,52 @@
+# Landing Page Project
+[![dependencies](https://img.shields.io/badge/dependencies-none-brightgreen)](dependencies) 
+[![dependencies](https://img.shields.io/badge/version-v1.0.0-brightgreen)](dependencies) 
+[![dependencies](https://img.shields.io/badge/licence-MIT-brightgreen)](dependencies) 
+## Table of Contents
+
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Support](#support)
+* [License](#license)
+
+## Description
+
+This work is base on a landing webapp by Udacity, containing some css styling and HTML. The goal is to turn  the static page into a dynamic experience,
+adding content to the'js/app.js' javascript file.
+Using of s gained techniques, earlier in 2020, by Analysis of some  [JQuery 3.2.1](https://code.jquery.com/jquery-3.2.1.js "JQuery source code") parts and [nmpjs es6-set](https://www.npmjs.com/package/es6-set "npmjs source code") polyfill, Mainly the ways of calculating height and how  to change a function context (using call, apply methods). concerned functions ```addClass``` and ```removeClass```.
+
+the app.js includes three main functions:
+
+- ```getComputedHeight``` using getComputedStyle to get computed style.
+- ```isLoadededInViewport``` uses calculated element's height, ```window.innerHeight``` and ```getBoundingClientRect``` to say if the element is in the viewport.       
+- ```document scroll event``` is used to detect scrolling. 
+
+### Alternatives
+
+to implement similar behaviour: IntersectionObserver could be use. However for the sake of backward compatibility, we use the old school javascript.
+Same is to say for ```classList``` property.
+
+[here ](https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API "Itersection observer API MDN Documentation")
+## Installation
+No installation required, create an empty folder and clone the project using git
+
+```
+git clone https://github.com/anaruz-source/landing-page.git <!--- fake --->
+
+```
+## Usage
+Link the app.js to the html index.html
+
+```
+<script src="js/app.js"></script>
+```
+## Support
+
+For any feedback, support, technical issues, security issues
+
+[Contact info](support@landingpage.org)
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/) license
---
+++ js/app.js
@@ -0,0 +1,267 @@
+/**
+ *
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ *
+ * Dependencies: None
+ *
+ * JS Version: ES2015/ES6
+ *
+ * JS Standard: ESlint
+ *
+ */
+
+/**
+ * Define Global Variables
+ *
+ */
+
+ /****
+  
+        * CODE IS LINT-FREE \\\^^^^^_^^^^^///
+  
+  ****/
+
+'use strict'
+
+const docElm = document.documentElement,
+
+    secs = document.getElementsByTagName('section'),
+
+    ul = document.getElementById('navbar__list'),
+
+    pageHeader = document.getElementsByClassName('page__header')[0];
+
+
+/**
+ * End Global Variables
+ * Start Helper Functions
+ *
+ */
+
+
+let addClass = function (className) { // Using  declaration function/function expression because, arrow functions resolve 'this'  to enclosing lexical scope
+                                     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+                                     // changing function's scope
+
+    let classList = this.className ? this.className : '';
+
+    if (classList.indexOf(className) === -1) {
+
+        classList += ' ' + className;
+
+        this.className = classList.trim();
+
+
+    }
+},
+
+    removeClass = function (className) { // Using  declaration function/Function Expression  because, arrow functions resolve 'this'  to enclosing lexical scope
+                                        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+
+        let classList = this.className ? this.className : '';
+
+        if (classList.indexOf(className) !== -1) { //stop here if className doesn't contain the required className
+
+            let classArray = classList.split(/\s+/), //split based on regex, at least one space seperates classNames 
+
+                pos = classArray.indexOf(classArray);
+
+            classArray.splice(pos, 1, '');
+
+            classList = classArray.join(' ');
+
+            this.className = classList;
+
+
+        }
+
+
+    },
+
+
+    getComputedHeight = (elm) => {
+
+        if (elm === undefined) return 0; //avoiding any unnecessary calculation if element parameter is undefined
+
+        return parseFloat(getComputedStyle(elm).height); // parseFloat converts string to a float stripping off the px unit
+        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseFloat
+
+    },
+
+
+    /**
+     * End Helper Functions
+     * Begin Main Functions
+     *
+     */
+
+
+    // build the nav
+
+
+    // Add class 'active' to section when near top of viewport
+
+
+    // Scroll to anchor ID using scrollTO event
+
+
+
+        // Inspired by https://medium.com/talk-like/detecting-if-an-element-is-in-the-viewport-jquery-a6a4405a3ea2
+        // code adapted to my special use
+    isLoadedInViewport = (elm) => {
+
+        let elmHeight = getComputedHeight(elm),
+
+            winHeight = window.innerHeight,
+
+            docElmY = docElm.scrollTop,
+
+            elmRect = elm.getBoundingClientRect(), // returns the size (width, height) of the element and its position (top, bottom)relative to viewport!
+
+
+ 
+
+             // this to get a fixed top value of the section in the document.
+            //(0, 0) is moved to the topmost of the document instead of the topmost of the viewport
+
+            elmCoors = {
+                top: elmRect.top + docElmY,
+                bttm: elmRect.top + docElmY + elmHeight
+            },
+            
+
+            //When scrollTop is used on the root element (the <html> element), the scrollY of the window is returned
+           // https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollTop
+          
+            winCoors = {
+                top: docElmY,
+                bttm: docElmY + winHeight
+            }; 
+
+
+
+        return winCoors.top <= elmCoors.top && winCoors.bttm + elmHeight / 2 >= elmCoors.bttm || // element loaded in the viewport, or its half is visible
+           
+            winCoors.top < elmCoors.bttm && winCoors.top > elmCoors.top;  // a part of element is still visible in the viewport
+
+    },
+
+    createMenu = (elms) => {
+
+
+        let liElems = '';
+
+        for (let i = 0; i < elms.length; i++) { // we can use Array.prototype.forEach.call instead to change the 'this' reference of forEach to be 'this' of HMTLCollection
+                                                 // as we did for addClass and removeClass
+            liElems += `<li><a href="#${secs[i].id}"  data-link ="${secs[i].id}">  ${secs[i].getAttribute('data-nav')}  </a></li>`;
+        }
+
+        return liElems;
+    };
+
+/**
+ * End Main Functions
+ * Begin Events
+ *
+ */
+
+// Build menu
+
+// Scroll to section on link click
+
+// Set sections as active
+
+
+(function () {
+
+
+    ul.innerHTML = createMenu(secs);
+
+
+    let timeoutId = null;
+
+    timeoutId = setTimeout(() => pageHeader.style.visibility = 'hidden', 5000); // page header disappears after 5000 ms, 5s
+    
+    pageHeader.addEventListener('click', function(e){ //event delegation to the parent node
+
+          e.preventDefault();
+
+          if(e.target.nodeName === 'A') {
+
+             document.getElementById(e.target.dataset.link).scrollIntoView(true);
+
+            
+          }
+
+    });
+
+
+    document.addEventListener('scroll', () => {
+
+
+        let nbOfSecs = secs.length;
+
+        pageHeader.style.visibility = 'visible';
+
+
+        clearTimeout(timeoutId);
+
+
+        for (let i = 0; i < nbOfSecs; i++) {
+
+            if (isLoadedInViewport(secs[i])) {
+
+
+                addClass.call(secs[i], 'active');
+                addClass.call(document.querySelector(`li > [href="#${secs[i].id}"]`).parentElement, 'active');
+
+
+                for (let j = 0; j < nbOfSecs; j++) {
+
+                    if (j !== i) { // this check is necessary, because isLoadedInViewport could return true for both current and next sections, or current and previousone
+                        // to make sure, if scrolling down next will have 'active' class, otherwise, previous get 'active'
+                        // this behaviour is due to the fact we want smooth scrolling transition from one section to the other, 
+                        // thus we visualize background moving from one menu  to the other
+
+
+                        removeClass.call(secs[j], 'active');
+                        removeClass.call(document.querySelector(`li > [href="#${secs[j].id}"]`).parentElement, 'active');
+
+
+                    }
+
+
+                }
+
+
+            } else { // to remove 'active' class for others to which isLoadedInViewport returns false if any
+
+
+                removeClass.call(secs[i], 'active');
+                removeClass.call(document.querySelector(`li > [href="#${secs[i].id}"]`).parentElement, 'active');
+            }
+
+            if (getComputedStyle(pageHeader).display === 'block') { // only on small device, otherwise, display: flex;
+
+
+                if (secs[i].className.indexOf('active') !== -1) {
+
+                    let pageHeaderScH = pageHeader.scrollHeight;
+
+                    pageHeader.scrollTop = pageHeaderScH * i / nbOfSecs; // make the menu bar scroll to the highlighted section 1 to 5;
+
+                }
+
+            }
+        }
+
+
+        timeoutId = setTimeout(() => pageHeader.style.visibility = 'hidden', 5000);
+
+
+    });
+
+})();
